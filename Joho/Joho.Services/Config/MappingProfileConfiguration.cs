de0daa9c8f9949cbebdaeb1355558fffa0a5afa6
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:05:57 +0530

    MappingProfileConfiguration class added

---
+++ Joho/Joho.Services/Config/MappingProfileConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Joho.Services.DTO.Response;
+using Joho.Services.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.Config
+{
+    public class MappingProfileConfiguration : Profile
+    {
+        public MappingProfileConfiguration()
+        {
+          
+            CreateMap<UserInfoReq, UserInfoRes>().ReverseMap();
+            CreateMap<SalaryInfoReq, SalaryInfoRes>().ReverseMap();
+        }
+    }
+}
