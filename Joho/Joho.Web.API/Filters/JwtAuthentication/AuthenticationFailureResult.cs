dd623f24fe6dc4f15b9441611339d2bd7ee7e47d
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 17:23:26 +0530

    dependency classes added

---
+++ Joho/Joho.Web.API/Filters/JwtAuthentication/AuthenticationFailureResult.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace Joho.Web.API.Filters.JwtAuthentication
+{
+    public class AuthenticationFailureResult : IHttpActionResult
+    {
+        public AuthenticationFailureResult(string reasonPhrase, HttpRequestMessage request)
+        {
+            ReasonPhrase = reasonPhrase;
+            Request = request;
+        }
+
+        public string ReasonPhrase { get; }
+
+        public HttpRequestMessage Request { get; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                RequestMessage = Request,
+                ReasonPhrase = ReasonPhrase,
+              //  Content = new StringContent(Utility.ResultStatus(false, ReasonPhrase, string.Empty))
+              Content=new StringContent (ReasonPhrase)
+            };
+
+            return response;
+        }
+    }
+}
