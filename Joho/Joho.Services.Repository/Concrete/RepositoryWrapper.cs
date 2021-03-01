52cf19c294d41a0fe58a4280c618e3a8c7c76135
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:56:37 +0530

    RepositoryWrapper class added

---
+++ Joho/Joho.Services.Repository/Concrete/RepositoryWrapper.cs
@@ -0,0 +1,57 @@
+﻿using Joho.Services.Entities;
+using Joho.Services.Repository.Abstract;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.Repository.Concrete
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private DataBaseConnector _dataBaseConnector;
+        private readonly IConfiguration _configuration;
+       
+        private ISalaryDetailsRepository _salary;
+
+
+        private IUserDetailsRepository _user;
+        public RepositoryWrapper(DataBaseConnector dataBaseConnector, IConfiguration configuration)
+        {
+            _configuration = configuration; ;
+            _dataBaseConnector = dataBaseConnector;
+        }
+
+        public IUserDetailsRepository User
+        {
+            get
+            {
+                if(_user==null)
+                {
+                    _user = new UserDetailsRepository(_dataBaseConnector);
+                }
+                return _user;
+            }
+        }
+
+
+        public ISalaryDetailsRepository Salary
+        {
+            get
+            {
+                if (_salary == null)
+                {
+                    _salary = new SalaryDetailsRepository(_dataBaseConnector);
+                }
+
+                return _salary;
+            }
+        }
+
+
+
+     
+    
+      
+    }
+}
