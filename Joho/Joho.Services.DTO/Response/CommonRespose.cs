2fd70bdaafbf5354a337cf5cdf875ad1e99d784d
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:25:43 +0530

    CommonRespose info class added

---
+++ Joho/Joho.Services.DTO/Response/CommonRespose.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.DTO.Response
+{
+    public class CommonRespose
+    {
+        public int Id { get; set; }
+        public Boolean Status { get; set; }
+        public string Message { get; set; }
+    }
+}
