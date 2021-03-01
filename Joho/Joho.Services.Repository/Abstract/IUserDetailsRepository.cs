8ca0bd4b34b16ecd3c112aef8df12cafe3d5fc06
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:51:02 +0530

    dependency classes added

---
+++ Joho/Joho.Services.Repository/Abstract/IUserDetailsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Joho.Services.Entities.Models;
+using System.Threading.Tasks;
+
+namespace Joho.Services.Repository.Abstract
+{
+    public interface IUserDetailsRepository
+    {
+       public Task<IEnumerable<UserInfoRes>> GetUserDetailsList(int userId);
+        public Task<IList<UserInfoRes>> GetAllUserDetails();
+        public Task<int> SaveuserDetails(UserInfoReq userInfoReq);
+        public Task<int> UpdateUserDetailsById(UserInfoReq userInfoReq);
+        public Task<int> DeleteUserDetailsById(int userId);
+
+    }
+}
+
+
+
+
