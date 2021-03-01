a031626aa01a4b5abb55bc51fa5f309d38268a9e
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:50:44 +0530

    dependency classes added

---
+++ Joho/Joho.Services.Repository/Abstract/ISalaryDetailsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Joho.Services.Entities.Models;
+using System.Threading.Tasks;
+
+namespace Joho.Services.Repository.Abstract
+{
+   public  interface ISalaryDetailsRepository
+    {
+        public Task<IEnumerable<SalaryInfoRes>> GetSalaryDetailsList(int sId);
+        public Task<IList<SalaryInfoRes>> GetAllSalaryDetails();
+        public Task <IList<usersalInfo>> GetUserSalaryDetails();
+        public Task<int> SaveSalaryDetails(SalaryInfoReq salaryInfoReq);
+        public Task<int> UpdateSalaryDetailsById(SalaryInfoReq salaryInfoReq);
+        public Task<int> DeleteSalryDetailsById(int sId);
+    }
+}
+
+
