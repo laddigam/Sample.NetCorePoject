a7898a2b4de274053dad5cd4f722fd0961f7a0c7
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:36:59 +0530

    UserInfoRes class added

---
+++ Joho/Joho.Services.Entities/Models/UserInfoRes.cs
@@ -0,0 +1,17 @@
+﻿using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.Entities.Models
+{
+   public class UserInfoRes
+    {
+        public int id { get; set; }
+        public string uname { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        public string designation { get; set; }
+    }
+}
