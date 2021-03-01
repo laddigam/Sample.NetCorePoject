96120c9e0bdca64b7c3e146fbb50c9a25fbbebbc
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:37:21 +0530

    salaryinfo class added

---
+++ Joho/Joho.Services.Entities/Models/usersalInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.Entities.Models
+{
+    public class usersalInfo
+    {
+        public int id { get; set; }
+        public string uname { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        public string designation { get; set; }
+        public int sid { get; set; }
+        public decimal basic { get; set; }
+        public decimal hra { get; set; }
+        public decimal da { get; set; }
+        public decimal oa { get; set; }
+        public decimal gross { get; set; }
+        public int u_id { get; set; }
+    }
+}
