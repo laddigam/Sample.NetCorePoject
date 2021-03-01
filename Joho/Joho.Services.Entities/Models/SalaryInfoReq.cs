10ce683d165b4fb4e30873aa810729b49ebfb2a0
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:35:41 +0530

    salaryinfo class added

---
+++ Joho/Joho.Services.Entities/Models/SalaryInfoReq.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.Entities.Models
+{
+    public class SalaryInfoReq
+    {
+
+        public int id { get; set; }
+        public decimal basic { get; set; }
+        public decimal hra { get; set; }
+        public decimal da { get; set; }
+        public decimal oa { get; set; }
+        public decimal gross { get; set; }
+        public int u_id { get; set; }
+    }
+}
