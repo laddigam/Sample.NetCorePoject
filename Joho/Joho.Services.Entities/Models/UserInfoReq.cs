f117981eb9ac2199a66a2370c4b427d29c7be1fd
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:36:27 +0530

    UserInfoReq added

---
+++ Joho/Joho.Services.Entities/Models/UserInfoReq.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joho.Services.Entities.Models
+{
+   public  class UserInfoReq
+    {
+        public int id { get; set; }
+        public string uname { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+        public string  address { get; set; }
+        public string designation { get; set; }
+    }
+}
