d597a624b2d57279326803e2dd2e58246c6ef942
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 16:50:21 +0530

    dependency classes added

---
+++ Joho/Joho.Services.Repository/Abstract/IRepositoryWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Joho.Services.Repository.Abstract
+{
+    public interface IRepositoryWrapper
+    {
+       
+        
+        IUserDetailsRepository User { get; }
+        ISalaryDetailsRepository Salary { get; }
+    }
+}
