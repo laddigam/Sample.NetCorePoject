2471811a5a2265c4a632963ce8d2698ef7f2a5c4
sumanjali.l <sumanjali.l@thinkpalm.com>
2021-03-01 15:53:34 +0530

    added user details

---
+++ Joho/Joho.Services/Abstract/IUserDetailsManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Joho.Services.DTO.Response;
+using System.Threading.Tasks;
+using Joho.Services.Entities.Models;
+
+namespace Joho.Services.Abstract
+{
+    public interface IUserDetailsManager
+    {
+
+        public Task<IEnumerable<UserInfoRes>> GetUserDetailsList(int userId);
+        public Task<IList<UserInfoRes>> GetAllUserDetails();
+        public Task<CommonRespose> SaveuserDetails(UserInfoReq userInfoReq);
+        public Task<CommonRespose> UpdateUserDetailsById(UserInfoReq userInfoReq);
+        public Task<CommonRespose> DeleteUserDetailsById(int userId);
+    }
+}
+
+
+
+
